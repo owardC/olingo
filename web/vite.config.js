fa2ce07faf72cc00cbe1bb3dff103eeb78f0ebbc
owardC <97756819+owardC@users.noreply.github.com>
2026-06-06 20:43:07 -0400

    fix: use base path only in production build

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/olingo/',
+  base: command === 'build' ? '/olingo/' : '/',
   server: {
     port: 5173
   }
-})
+}))
